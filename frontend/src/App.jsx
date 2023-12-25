70e803c9d4d9cb33a568433f110b2a36fd960b62
EshuDevs <yamindevs@gmail.com>
2023-12-25 14:44:57 +0100

    added main component to container

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,14 @@
 import './App.css'
 import Header from './containers/Header.jsx'
 import Banner from './components/Banner.jsx'
+import  Main from './containers/Main.jsx'
 
 function App() {
 
   return (
     <>
       <Header/>
-      <Banner/>
+      <Main/>
     </>
   )
 }

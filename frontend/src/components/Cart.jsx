934e9a1cf75f4986f63bbefe288332c5347cf56f
EshuDevs <yamindevs@gmail.com>
2023-12-27 16:33:53 +0100

    styled the card Items

--- frontend/src/components/Cart.jsx
+++ frontend/src/components/Cart.jsx
@@ -2,33 +2,6 @@ import { Fragment, } from 'react'
 import { Dialog, Transition } from '@headlessui/react'
 import { XMarkIcon } from '@heroicons/react/24/outline'
 
-const products = [
-
-
-  {
-    id: 1,
-    name: 'Throwback Hip Bag',
-    href: '#',
-    color: 'Salmon',
-    price: '$90.00',
-    quantity: 1,
-    imageSrc: 'https://tailwindui.com/img/ecommerce-images/shopping-cart-page-04-product-01.jpg',
-    imageAlt: 'Salmon orange fabric pouch with match zipper, gray zipper pull, and adjustable hip belt.',
-  },
-  {
-    id: 2,
-    name: 'Medium Stuff Satchel',
-    href: '#',
-    color: 'Blue',
-    price: '$32.00',
-    quantity: 1,
-    imageSrc: 'https://tailwindui.com/img/ecommerce-images/shopping-cart-page-04-product-02.jpg',
-    imageAlt:
-      'Front of satchel with blue canvas body, black straps and handle, drawstring top, and front zipper pouch.',
-  },
-
-]
-
 export default function Cart({ cartOpen, setCartOpen, addToCart }) {
 
   return (
@@ -78,15 +51,43 @@ export default function Cart({ cartOpen, setCartOpen, addToCart }) {
 
                       <div className="mt-8">
                         <div className="flow-root">
-                        <ul className="grid grid-cols-3 gap-6">
-                          {addToCart.map((item) => (
-                          <li className='flex flex-col items-center' key={item.id}>
-                              <img src={item.image} alt={item.title} style={{ maxWidth: '100px', maxHeight: '100px' }}/>
-                              <strong>{item.title}</strong>
-                              <p className="mt-auto">Price: ${item.price}</p>
-                          </li>
-                          ))}
-                      </ul>
+                        <ul role="list" className="-my-6 divide-y divide-gray-200">
+                            {addToCart.map((item) => (
+                              <li key={item.id} className="flex py-6">
+                                <div className="h-24 w-24 flex-shrink-0 overflow-hidden rounded-md">
+                                  <img
+                                    src={item.image}
+                                    alt={item.title}
+                                    className="h-full object-center"
+                                  />
+                                </div>
+
+                                <div className="ml-4 flex flex-1 flex-col">
+                                  <div>
+                                    <div className="flex justify-between text-base font-medium text-gray-900">
+                                      <h3>
+                                        <a href={item.href}>{item.title}</a>
+                                      </h3>
+                                      <p className="ml-4">{item.price}€</p>
+                                    </div>
+                                    <p className="mt-1 text-sm text-gray-500">{item.color}</p>
+                                  </div>
+                                  <div className="flex flex-1 items-end justify-between text-sm">
+                                    <p className="text-gray-500">Qty {item.quantity}</p>
+
+                                    <div className="flex">
+                                      <button
+                                        type="button"
+                                        className="font-medium text-beige-dark hover:text-beige"
+                                      >
+                                        Remove
+                                      </button>
+                                    </div>
+                                  </div>
+                                </div>
+                              </li>
+                            ))}
+                          </ul>
                         </div>
                       </div>
                     </div>
--- frontend/src/components/ProductCard.jsx
+++ frontend/src/components/ProductCard.jsx
@@ -8,14 +8,14 @@ export default function ProductCard({mensClothingData, setAddToCart}) {
 
     return(
         <>
-            <ul className="grid grid-cols-3 gap-6">
+            <ul className="grid grid-cols-3 gap-6 h-full">
                 {mensClothingData.map((item) => (
-                <li className='flex flex-col items-center' key={item.id}>
+                <li className='flex flex-col items-center ' key={item.id}>
                     <img src={item.image} alt={item.title} style={{ maxWidth: '100px', maxHeight: '100px' }}/>
                     <strong>{item.title}</strong>
-                    <div className="flex justify-between mt-auto w-full ">
+                    <div className="flex justify-between mt-auto w-full">
                         <p className="mt-auto mr-auto">Price: ${item.price}</p>
-                        <button className="border border-solid" onClick={() => handleAddToCart(item)}>add to cart</button>
+                        <button className="border border-solid bg-beige-dark hover:bg-beige text-white rounded-md px-2 py-0.5 mt-auto" onClick={() => handleAddToCart(item)}>add to cart</button>
                     </div>
                 </li>
                 ))}
